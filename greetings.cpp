1261a69ae1f135dab4e73b536f5d46eb7a92bb8a
zroja <32425057+zroja@users.noreply.github.com>
2017-10-09 17:11:26 -0400

    Create greetings.cpp

---
+++ greetings.cpp
@@ -0,0 +1,16 @@
+ 1 //Zahin Roja
+  2 //September 10, 2017
+  3 //Lab 1- First C++ program continue
+  4
+  5 #include <iostream>
+  6 using namespace std;
+  7
+  8 int main()
+  9 {
+ 10         cout << "Greetings Earth"                           << endl;
+ 11         cout << "We Come in      Peace"                     << endl;
+ 12         cout << "To Teach you C++"                          << endl;
+ 13         cout << "Let's change the world with our creations" << endl;
+ 14         cout << "We are the new innovators"                 << endl;
+ 15         return 0;
+ 16 }
